9014d4fd6b5d696064d4e5c4e3fbdf1a35079e85
Federico Slongo <73640124+FedericoSlongo@users.noreply.github.com>
2022-11-07 17:41:24 +0100

    Wan or lan?

--- Ping everyone/Program.cs
+++ Ping everyone/Program.cs
@@ -11,22 +11,58 @@ namespace Ping_everyone
     {
         static void Main(string[] args)
         {
+            //Variables be like
             StreamWriter sw = new StreamWriter("ip-v4.txt");
             send_ping pinger = new send_ping();
-            int[] ipv4 = new int[4] {1,1,1,0};
-            bool[] ipv4_bool = new bool[4] {false,false,false,true};
-            for (double i = 0; i < 4294967296; i++)
-            {
-                if (ipv4[0] == 255)
-                    break;
-                
-                for (int coc = 0; coc < 255; coc++)
+            char type = ' ';
+            int[] ipv4 = new int[4] { 1, 0, 0, 0 };
+
+            Console.Write("You want to ping the [L]AN or [W]AN? ");
+            type = Convert.ToChar(Console.ReadLine().ToLower());
+            Console.Clear();
+            if (type == 'w')
+                for (double i = 0; i < 4294967296; i++)
                 {
-                    for (int z = 0; z < 255; z++)
+                    if (ipv4[0] == 255)
+                        break;
+
+                    for (int coc = 0; coc < 255; coc++)
                     {
+                        for (int z = 0; z < 255; z++)
+                        {
+                            for (int a = 0; a < 255; a++)
+                            {
+                                ipv4[3]++;
+                                //Prints out the ip it's pinging
+                                Console.WriteLine($"{ipv4[0]}.{ipv4[1]}.{ipv4[2]}.{ipv4[3]}");
+                                writer_pinger(sw, ipv4, pinger);
+                            }
+                            ipv4[3] = 0;
+                            ipv4[2]++;
+                        }
+                        ipv4[2] = 0;
+                        ipv4[1]++;
+                    }
+                    ipv4[1] = 0;
+                    ipv4[0]++;
+
+                    //Only ping external ips
+                    if (ipv4[0] == 192 || ipv4[0] == 127)
+                        continue;
+                    //Pings and write to file the result
+                    writer_pinger(sw, ipv4, pinger);
+                }
+            else if (type == 'l')
+            {
+                //10.0.0.0–10.255.255.255
+                ipv4[0] = 10;
+                while (true)
+                {
+                    for(int z = 0; z < 255; z++)
+                        {
                         for (int a = 0; a < 255; a++)
                         {
-                             ipv4[3]++;
+                            ipv4[3]++;
                             //Prints out the ip it's pinging
                             Console.WriteLine($"{ipv4[0]}.{ipv4[1]}.{ipv4[2]}.{ipv4[3]}");
                             writer_pinger(sw, ipv4, pinger);
@@ -36,21 +72,58 @@ namespace Ping_everyone
                     }
                     ipv4[2] = 0;
                     ipv4[1]++;
+                    if (ipv4[1] == 255)
+                        break;
                 }
-                ipv4[1] = 0;
-                ipv4[0]++;
-
-                //Only ping external ips
-                if (ipv4[0] == 192 || ipv4[0] == 127)
-                    continue;
-                //Pings and write to file the result
-                writer_pinger(sw, ipv4, pinger);
+                //172.16.0.0–172.31.255.255
+                ipv4[0] = 172;
+                ipv4[1] = 16;
+                while (true)
+                {
+                    for (int z = 0; z < 255; z++)
+                    {
+                        for (int a = 0; a < 255; a++)
+                        {
+                            ipv4[3]++;
+                            //Prints out the ip it's pinging
+                            Console.WriteLine($"{ipv4[0]}.{ipv4[1]}.{ipv4[2]}.{ipv4[3]}");
+                            writer_pinger(sw, ipv4, pinger);
+                        }
+                        ipv4[3] = 0;
+                        ipv4[2]++;
+                    }
+                    ipv4[2] = 0;
+                    ipv4[1]++;
+                    if (ipv4[1] == 31)
+                        break;
+                }
+                //192.168.0.0–192.168.255.255
+                ipv4[0] = 192;
+                ipv4[1] = 168;
+                while (true)
+                {
+                    for (int a = 0; a < 255; a++)
+                    {
+                        ipv4[3]++;
+                        //Prints out the ip it's pinging
+                        Console.WriteLine($"{ipv4[0]}.{ipv4[1]}.{ipv4[2]}.{ipv4[3]}");
+                        writer_pinger(sw, ipv4, pinger);
+                    }
+                    ipv4[3] = 0;
+                    ipv4[2]++;
+                    ipv4[2] = 0;
+                    ipv4[1]++;
+                    if (ipv4[2] == 255)
+                        break;
+                }
             }
+            else
+                Console.WriteLine("Not a valid option");
             Console.ReadKey();
             sw.Dispose();
         }
 
-        public static void writer_pinger(StreamWriter sw, int[] ipv4 , send_ping pinger)
+        public static void writer_pinger(StreamWriter sw, int[] ipv4, send_ping pinger)
         {
             Console.WriteLine($"{ipv4[0]}.{ipv4[1]}.{ipv4[2]}.{ipv4[3]}");
             //Pings and write to file the result
